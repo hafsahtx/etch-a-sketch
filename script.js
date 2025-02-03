6280701da0b9a9a90af50199ba02092e3c8ccc3e
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-02-03 12:50:08 +0000

    Fixed spacing using container width and removing border

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const container = document.querySelector("div");
 for(i=1;i<=256;i++){
     const item = document.createElement("div");
-    let w = container.clientWidth/16;
+    let w = container.offsetWidth/16;
     item.textContent = `${i}`;
-    item.style.width = "78px";
+    item.style.width = `${w}px`;
     item.style.height = "35px";
-    item.style.border = "1px solid black";
     item.style.textAlign = "center";
     container.appendChild(item);
 
 }
+
 container.addEventListener("mouseover", (event)=>{
     event.target.style.background = "blue";
 })
 
-document.body.insertBefore(btn,container);
+
+//document.body.insertBefore(btn,container);
