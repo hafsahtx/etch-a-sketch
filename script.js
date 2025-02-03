3acbfcdd94a20b004074ad0de830e8f7556d4b67
Hafsa <148873058+hafsahtx@users.noreply.github.com>
2025-02-03 13:18:03 +0000

    Add button and create a new grid based on input

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-const container = document.querySelector("div");
-for(i=1;i<=256;i++){
-    const item = document.createElement("div");
-    let w = container.offsetWidth/16;
-    item.textContent = `${i}`;
-    item.style.width = `${w}px`;
-    item.style.height = "35px";
-    item.style.textAlign = "center";
-    container.appendChild(item);
-
+function createGrid(number){
+    let squared = number*number;
+    for(i=1;i<=squared;i++){
+        const item = document.createElement("div");
+        let w = container.offsetWidth/number;
+        item.textContent = `${i}`;
+        item.style.width = `${w}px`;
+        item.style.height = "35px";
+        item.style.textAlign = "center";
+        container.appendChild(item);
+    }
 }
-
+const container = document.querySelector("div");
+createGrid(16);
 container.addEventListener("mouseover", (event)=>{
     event.target.style.background = "blue";
 })
+let btn = document.createElement("button");
+btn.textContent = "button";
 
-
-//document.body.insertBefore(btn,container);
+btn.addEventListener("click",()=>{
+    let num = prompt("number of squares for new grid");
+    container.innerHTML = "";
+    createGrid(num);
+})
+document.body.insertBefore(btn,container);
